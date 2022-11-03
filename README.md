9c7d48a7b58b8938bf63cc61e478f72e9214e70e
Georgios Tzanakis <g.tzanakis@ferryhopper.com>
2022-11-03 10:28:40 +0200

    Initial commit for backup purposes; has issues

---
+++ README.md
@@ -0,0 +1,9 @@
+# Setup
+
+## Prerequisites
+
+A `.env` file as per `.env.templates`. Among others, it should include a google secret json key as a one-liner
+
+## Installation
+
+from the root folder run `pip install -e .`. Then import the module by `import sapbulkgenerator`
---
+++ setup.py
@@ -0,0 +1,71 @@
+
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name="ReconcileBqData",
+    version="0.0.1",  # Required
+    description="Reconcile booking documents data ingested in BQ",  # Optional
+    long_description=long_description,  # Optional
+    long_description_content_type="text/markdown",  # Optional (see note above)
+    # url="https://github.com/pypa/sampleproject",  # Optional
+    author="Georgios Tzanakis",
+    author_email="g.tzanakis@ferryhopper.com",
+    classifiers=[  # Optional
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    # When your source code is in a subdirectory under the project root, e.g.
+    # `src/`, it is necessary to specify the `package_dir` argument.
+    package_dir={"": "src"},  # Optional
+    # You can just specify package directories manually here if your project is
+    # simple. Or you can use find_packages().
+    #
+    # Alternatively, if you just want to distribute a single Python file, use
+    # the `py_modules` argument instead as follows, which will expect a file
+    # called `my_module.py` to exist:
+    #
+    #   py_modules=["my_module"],
+    #
+    packages=find_packages(where="src"),  # Required
+    # Specify which Python versions you support. In contrast to the
+    # 'Programming Language' classifiers above, 'pip install' will check this
+    # and refuse to install the project if the version does not match. See
+    # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
+    python_requires=">=3.9, <4",
+    # This field lists other packages that your project depends on to run.
+    # Any package you put here will be installed by pip when your project is
+    # installed, so they must be valid existing projects.
+    #
+    # For an analysis of "install_requires" vs pip's requirements files see:
+    # https://packaging.python.org/discussions/install-requires-vs-requirements/
+    #install_requires=["peppercorn"],  # Optional
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). Users will be able to install these using the "extras"
+    # syntax, for example:
+    #
+    #   $ pip install sampleproject[dev]
+    #
+    #entry_points={  # Optional
+    #    "console_scripts": [
+    #        "dbdata=DBData:main",
+    #    ],
+    #},
+)
