2b4d06ae0b3d5919db526a61df0cb4e7da215b3c
Georgios Tzanakis <g.tzanakis@ferryhopper.com>
2022-11-03 20:09:25 +0200

    Refactored the handler

--- src/reconcilebqdata/queryhandler.py
+++ src/reconcilebqdata/queryhandler.py
@@ -1,7 +1,9 @@
 from dotenv import load_dotenv
 from pathlib import Path
+from sqlalchemy import Table
 from sqlalchemy.engine.base import Engine
-from typing import Literal, TypedDict, Optional, Tuple
+from sqlalchemy.sql.selectable import Select
+from typing import Literal, TypedDict, Tuple
 import datetime
 import json
 import os
@@ -20,21 +22,16 @@ MYSQL_DATABASE = os.environ["MYSQL_DATABASE"]
 MYSQL_URI = (
     f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DATABASE}"
 )
-TRIP_ID_MIN = 1800000
-TRIP_ID_MAX = 1928302
 
-QUERY_CATEGORIES_MYSQL = ("mysql_trips", "mysql_open", "mysql_cnx")
-QUERY_CATEGORIES_BQ = ("bq_trips",)
-QUERY_CATEGORIES = QUERY_CATEGORIES_BQ + QUERY_CATEGORIES_MYSQL
+# Always prefix categories by "mysql_" or "bigquery_"
+QUERY_CATEGORIES = ("mysql_trips", "mysql_open", "mysql_cnx", "bigquery_trips")
 
 BQ_PROJECT_ID = os.environ["BQ_PROJECT_ID"]
-BQ_CREDENTIALS_INFO = json.loads(
-    os.environ["GCP_KEY_TZANAKIS_BIGQUERY"]
-)
+BQ_CREDENTIALS_INFO = json.loads(os.environ["GCP_KEY_TZANAKIS_BIGQUERY"])
 
 
 class QueryResult(TypedDict):
-    trip_id: int
+    id: int
     booking_code: str
 
 
@@ -51,243 +48,237 @@ def time_function(func):
     return wrapper
 
 
+def get_timestamp() -> str:
+    now = (
+        datetime.datetime.now()
+        .replace(microsecond=0, tzinfo=datetime.timezone.utc)
+        .isoformat()
+    )
+    return now
+
+
+def get_table(engine: Engine, schema, table) -> Table:
+    return sa.Table(
+        table,
+        sa.MetaData(),
+        autoload=True,
+        autoload_with=engine,
+        schema=schema,
+    )
+
+
 class QueryHandler:
-    """
-    This class is used to further process the form responses data frame and generate invoices.
-
-    Args:
-        category (QUERY_CATEGORIES): One of predefined categories, like "mysql_trips", "bq_trips", etc; see definition of QUERY_CATEGORIES
-        trip_id_range (Tuple[int, int]): A tuple of the form (trip_id_min, trip_id_max)
-
-    Attributes:
-        category (QUERY_CATEGORIES): See definition in Args
-        trip_id_range (Tuple[int, int]): See definition in Args
-        results (Optional[QueryResult]): Dictionary where key/value is id/FH code
-    """
-
     def __init__(
-        self, category: Literal[QUERY_CATEGORIES], trip_id_range: Tuple[int, int]
+        self, query_category: Literal[QUERY_CATEGORIES], id_range: Tuple[int, int]
     ) -> None:
+        self._query_category = query_category
+        self._id_range = id_range
+        self.init_result()
 
-        self.category = category
-        self.trip_id_range = trip_id_range
+        self._engine = self.get_engine()
+        self._query = self.get_query()
+
+    def init_result(self) -> None:
         self.result = None
+        self.result_count_ids = None
+        self.result_count_booking_codes = None
+        self.updated_at = None
 
-        self._trip_id_min = trip_id_range[0]
-        self._trip_id_max = trip_id_range[1]
-        self._engine = self._get_engine()
-        self._query = self._get_query()
-        self._updated_at = None
-        self._filename = self.get_output_filename()
-
-    def get_output_filename(self):
-        if not self.result:
-            return None
-
-        filename = f"{self._updated_at}_{self.category}_{self._trip_id_min:09}-{self._trip_id_max:09}.pickle"
-        return filename
+    def update(self) -> None:
+        now = get_timestamp()
+        result = self.execute_query()
+        self.result = result
+        self.result_count_ids = len(result)
+        self.result_count_booking_codes = len(set(result.values()))
+        self.updated_at = now
 
     @property
-    def category(self):
-        return self._category
+    def query_category(self):
+        return self._query_category
 
-    @category.setter
-    def category(self, new_category):
-        if not new_category in QUERY_CATEGORIES:
+    @query_category.setter
+    def query_category(self, new_query_category: str):
+
+        if new_query_category not in QUERY_CATEGORIES:
             raise ValueError(f"Category should be in {QUERY_CATEGORIES}")
-        self._category = new_category
-        self.result = None
-        self._filename = self.get_output_filename()
+
+        if self.query_category != new_query_category:
+            self.init_result()
+            self._query_category = new_query_category
 
     @property
-    def trip_id_range(self):
-        return self._trip_id_range
+    def id_range(self):
+        return self._id_range
 
-    @trip_id_range.setter
-    def trip_id_range(self, new_range: Tuple[int, int]):
-        trip_id_min = new_range[0]
-        trip_id_max = new_range[1]
-        if 0 < trip_id_min < trip_id_max:
-            self._trip_id_range = new_range
-            self._trip_id_min = trip_id_min
-            self._trip_id_max = trip_id_max
-            self._filename = self.get_output_filename()
-            self.result = None
+    @id_range.setter
+    def id_range(self, new_range: Tuple[int, int]):
+        id_min = new_range[0]
+        id_max = new_range[1]
+        if 0 < id_min < id_max:
+            self._id_range = new_range
+            self.init_result()
         else:
             raise ValueError("Input (a,b) should satisfy 0 < a < b")
 
-    def _get_engine(self):
-        if self.category in QUERY_CATEGORIES_MYSQL:
-            engine_mysql = sa.create_engine(MYSQL_URI)
-            return engine_mysql
+    def get_engine(self) -> Engine:
+        if self.query_category.startswith("mysql_"):
+            engine = self._engine_mysql()
+        elif self.query_category.startswith("bigquery_"):
+            engine = self._engine_bigquery()
         else:
-            engine_bq = sa.create_engine(
-                f"bigquery://{BQ_PROJECT_ID}", location="eu", credentials_info=BQ_CREDENTIALS_INFO
+            raise ValueError(
+                'query_category should be prefixed by "mysql_" or "bigquery_"'
             )
-            return engine_bq
+        return engine
 
-    def _get_query(self):
-
-        query = None
-
-        if self.category in QUERY_CATEGORIES_MYSQL:
-
-            table_rb = sa.Table(
-                "reservations_bookings",
-                sa.MetaData(),
-                autoload=True,
-                autoload_with=self._engine,
-            )
-
-            table_rbt = sa.Table(
-                "reservations_booked_trips",
-                sa.MetaData(),
-                autoload=True,
-                autoload_with=self._engine,
-            )
-
-            table_ror = sa.Table(
-                "reservations_open_requests",
-                sa.MetaData(),
-                autoload=True,
-                autoload_with=self._engine,
-            )
-
-            table_rcr = sa.Table(
-                "reservations_cancellation_requests",
-                sa.MetaData(),
-                autoload=True,
-                autoload_with=self._engine,
-            )
-
-            table_rr = sa.Table(
-                "reservations_reservations",
-                sa.MetaData(),
-                autoload=True,
-                autoload_with=self._engine,
-            )
-
-            if self.category == "mysql_trips":
-
-                query = (
-                    sa.select(
-                        [
-                            table_rbt.columns.id,
-                            table_rbt.columns.bookingCode,
-                        ]
-                    )
-                    .select_from(
-                        table_rbt.join(
-                            table_rb,
-                            table_rbt.columns.bookingCode == table_rb.columns.orderId,
-                            isouter=True,
-                        )
-                    )
-                    .where(
-                        (table_rb.columns.bookingState == "BOOKED")
-                        & (table_rbt.columns.replacedBy.is_(None))
-                        & (
-                            table_rbt.columns.id.between(
-                                self._trip_id_min, self._trip_id_max
-                            )
-                        )
-                    )
-                )
-
-            elif self.category == "mysql_open":
-
-                query = (
-                    sa.select(
-                        [table_ror.columns.bookedTripId, table_rb.columns.orderId]
-                    )
-                    .select_from(
-                        table_ror.join(
-                            table_rb,
-                            table_ror.columns.bookingId == table_rb.columns.id,
-                            isouter=True,
-                        )
-                    )
-                    .where(
-                        (
-                            table_ror.columns.bookedTripId.between(
-                                self._trip_id_min, self._trip_id_max
-                            )
-                        )
-                    )
-                )
-
-            elif self.category == "mysql_cnx":
-
-                query = (
-                    sa.select([table_rbt.columns.id, table_rbt.columns.bookingCode])
-                    .select_from(
-                        table_rcr.join(
-                            table_rr,
-                            table_rcr.columns.reservationId == table_rr.columns.id,
-                            isouter=True,
-                        ).join(
-                            table_rbt,
-                            (
-                                table_rbt.columns.bookingCode
-                                == table_rr.columns.bookingCode
-                            )
-                            & (
-                                table_rbt.columns.ReservationIndex
-                                == table_rr.columns.bookingIndex
-                            ),
-                            isouter=True,
-                        )
-                    )
-                    .where(
-                        (table_rbt.columns.replacedBy.is_(None))
-                        & (
-                            table_rbt.columns.id.between(
-                                self._trip_id_min, self._trip_id_max
-                            )
-                        )
-                    )
-                )
-
+    def get_query(self) -> Select:
+        if self.query_category == "mysql_trips":
+            return self._query_mysql_trips()
+        elif self.query_category == "mysql_open":
+            return self._query_mysql_open()
+        elif self.query_category == "mysql_cnx":
+            return self._query_mysql_cnx()
+        elif self.query_category == "bigquery_trips":
+            return self._query_bigquery_trips()
         else:
-            # self.category in QUERY_CATEGORIES_BQ
-            # self.category = "bq_trips"
-
-            table_base_reporting_trips = sa.Table(
-                "booking_documents_production.base_reporting_trips",
-                sa.MetaData(bind=self._engine),
-                autoload=True,
-            )
-            query = sa.select(
-                [sa.func.max(table_base_reporting_trips.columns.trip_id_trip)],
-                from_obj=table_base_reporting_trips,
-            )
-
-        return query
-
-    @category.setter
-    def category(self, new_category):
-        if not new_category in QUERY_CATEGORIES:
-            raise ValueError(f"Category should be in {QUERY_CATEGORIES}")
-        self._category = new_category
-        self.result = None
-
-    def _update_updated_at(self):
-        now = (
-            datetime.datetime.now()
-            .replace(microsecond=0, tzinfo=datetime.timezone.utc)
-            .isoformat()
-        )
-        self._updated_at = now
+            raise ValueError(f'Query category "{self.query_category}" is not valid.')
 
     @time_function
-    def update(self):
+    def execute_query(self) -> QueryResult:
 
         with self._engine.connect() as connection:
             results_proxy = connection.execute(self._query)
-            self.result = dict(results_proxy.fetchall())
-            self._update_updated_at()
+            result = QueryResult(results_proxy.fetchall())
+
+        return result
+
+    def _engine_mysql(self) -> Engine:
+
+        mysql_uri = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}"
+        engine = sa.create_engine(mysql_uri)
+        return engine
+
+    def _engine_bigquery(self) -> Engine:
+
+        engine = sa.create_engine(
+            f"bigquery://{BQ_PROJECT_ID}",
+            location="eu",
+            credentials_info=BQ_CREDENTIALS_INFO,
+        )
+
+        return engine
+
+    def _query_mysql_trips(self) -> Select:
+
+        table_rbt = get_table(self._engine, MYSQL_DATABASE, "reservations_booked_trips")
+        table_rb = get_table(self._engine, MYSQL_DATABASE, "reservations_bookings")
+
+        query = (
+            sa.select(
+                [
+                    table_rbt.columns.id,
+                    table_rbt.columns.bookingCode,
+                ]
+            )
+            .select_from(
+                table_rbt.join(
+                    table_rb,
+                    table_rbt.columns.bookingCode == table_rb.columns.orderId,
+                    isouter=True,
+                )
+            )
+            .where(
+                (table_rb.columns.bookingState == "BOOKED")
+                & (table_rbt.columns.replacedBy.is_(None))
+                & (table_rbt.columns.id.between(*self.id_range))
+            )
+        )
+
+        return query
+
+    def _query_mysql_open(self) -> Select:
+
+        table_ror = get_table(
+            self._engine, MYSQL_DATABASE, "reservations_open_requests"
+        )
+        table_rb = get_table(self._engine, MYSQL_DATABASE, "reservations_bookings")
+
+        query = (
+            sa.select(
+                [
+                    table_ror.columns.bookedTripId,
+                    table_rb.columns.orderId,
+                ]
+            )
+            .select_from(
+                table_ror.join(
+                    table_rb,
+                    table_ror.columns.bookingId == table_rb.columns.id,
+                    isouter=True,
+                )
+            )
+            .where((table_ror.columns.bookedTripId.between(*self.id_range)))
+        )
+
+        return query
+
+    def _query_mysql_cnx(self) -> Select:
+
+        table_rr = get_table(self._engine, MYSQL_DATABASE, "reservations_reservations")
+        table_rbt = get_table(self._engine, MYSQL_DATABASE, "reservations_booked_trips")
+        table_rcr = get_table(
+            self._engine, MYSQL_DATABASE, "reservations_cancellation_requests"
+        )
+
+        query = (
+            sa.select(
+                [
+                    table_rbt.columns.id,
+                    table_rbt.columns.bookingCode,
+                ]
+            )
+            .select_from(
+                table_rcr.join(
+                    table_rr,
+                    (table_rcr.columns.reservationId == table_rr.columns.id),
+                    isouter=True,
+                ).join(
+                    table_rbt,
+                    (table_rbt.columns.bookingCode == table_rr.columns.bookingCode)
+                    & (
+                        table_rbt.columns.ReservationIndex
+                        == table_rr.columns.bookingIndex
+                    ),
+                    isouter=True,
+                )
+            )
+            .where(
+                (table_rbt.columns.replacedBy.is_(None))
+                & (table_rbt.columns.id.between(*self.id_range))
+            )
+        )
+        return query
+
+    def _query_bigquery_trips(self) -> Select:
+
+        table_brt = get_table(
+            self._engine, "booking_documents_production", "base_reporting_trips"
+        )
+
+        query = sa.select(
+            table_brt.columns.trip_id_trip,
+            table_brt.columns.booking_code,
+        ).where(
+            (table_brt.columns.booking_meta_state == "BOOKED")
+            & (table_brt.columns.trip_id_trip.between(*self.id_range))
+        )
+
+        return query
+
 
 if __name__ == "__main__":
+    id_min = 1354900
+    range_ = (id_min, id_min + 1000)
 
-    foo = QueryHandler("bq_trips", (1350000, 1355000) )
+    handler = QueryHandler("mysql_cnx", range_)
     pass
